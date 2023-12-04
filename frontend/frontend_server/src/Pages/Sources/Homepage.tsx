83ef816d3fa5a3cc402d5cb3abb22df2f6f31cbe
Hubjerd <hubi.hey@vp.pl>
2023-12-05 00:34:41 +0100

    Added routing to React server, organized pages into pages folder

---
+++ frontend/frontend_server/src/Pages/Sources/Homepage.tsx
@@ -0,0 +1,8 @@
+
+
+function Homepage ()
+{
+    return (<>Default Homepage</>);
+} 
+
+export default Homepage;
--- frontend/frontend_server/src/main.tsx
+++ frontend/frontend_server/src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Login from './Login.tsx'
-import './login.css'
+import Login from './Pages/Sources/Login.tsx'
+import Homepage from './Pages/Sources/Homepage.tsx'
+import './Pages/CSS/login.css'
+import { BrowserRouter, Routes , Route } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Login />
+    <BrowserRouter>
+      <Routes>
+          <Route path="login" element = {<Login />} />
+          <Route path="/" element={<Homepage/>}/>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
