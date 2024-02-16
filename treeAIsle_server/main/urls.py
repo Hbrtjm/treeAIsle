b63bd6972272ddd824998a602fe8a2be12c271c7
Hubjerd <hubi.hey@vp.pl>
2024-02-16 21:36:37 +0100

    Backend bug fixed

--- treeAIsle_server/main/urls.py
+++ treeAIsle_server/main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import Login,login, api_index, UserView, Register
+from .views import Login, api_index, UserView, Register
 '''
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -7,15 +7,11 @@ from rest_framework_simplejwt.views import (
     TokenVerifyView
 )
 '''
+
 urlpatterns = [
     path('view_users',UserView.as_view()),
     path('api/',api_index),
     path('',api_index),
     path('api/login', Login), 
     path('api/register',Register),
-'''
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-'''
 ]
