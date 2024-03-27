dbee17133be8eb4577775fd86bbaacd1388ff80f
Hbrtjm <hubi.hey@vp.pl>
2024-03-27 23:50:15 +0100

    Saved changes

--- treeAIsle_server/main/serializers.py
+++ treeAIsle_server/main/serializers.py
@@ -1,6 +1,12 @@
 from rest_framework import serializers
 from .models import User
+from djoser.serializers import UserCreateSerializer
+from django.contrib.auth import get_user_model
 
+class UserCreateSerialzier(UserCreateSerializer):
+    class Meta(UserCreateSerializer.Meta):
+        model = User
+        
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
